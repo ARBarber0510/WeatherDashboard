c9b7ad0097a8e22b8fbb8f9a563ad6b7b7965bf5
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-08-06 12:14:59 -0500

    pseudocoded what js functions are supposed to do.

--- script.js
+++ script.js
@@ -12,5 +12,15 @@ $(button).on("click", function() {
     })
 })
 
+// Want to create a weather dashboard.
+// Left column is used to search for the weather in a specific city.
+// Local storage saves previous searches and lists them below search bar.
+// When a city is entered and search button clicked, the inputs current forecast appears on the right hand side.
+// Current weather conditions reflects temp, humidity, wind speed, and UV index.
+// UV index is color coded to reflect the severity of the conditions
+// Below the current weather conditions is the future forecast for the next 5 days.
+
+
+
 
 

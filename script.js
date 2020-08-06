249f1ab79032da06f5e317e208149095dfe8abfb
Alison Barber <65191513+ARBarber0510@users.noreply.github.com>
2020-08-06 12:29:21 -0500

    targeting forecast id.

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-$(button).on("click", function() {
-    var cityName = $("#searchInput").val();
+var cityName = $("#searchInput").val();
+// var savedCities = [];
+// var currentCity;
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=96598b0725a855977df1a03e8cab48c9";
+//     function searchCity() {
+//         savedCities = JSON.parse(localStorage.getItem("weatherCities"));
+//         var lastSearch;
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-    .then(function(response) {
-        console.log(response)
-    })
-})
+//         if (savedCities) {
+//             currentCity = savedCities[savedCities.length - 1];
+//             showPrevious();
+//             getCurrent(currentCity);
+//         } else {
+//             if (!navigator.geolocation) {
+//                 getCurrent("");
+//             }
+//             else {
+//                 navigator.geolocation.getCurrentPosition(success, error);
+//             }
+//         }
+//     }
+//     searchCity();
+
+    function getForecast(city) {
+
+        var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=96598b0725a855977df1a03e8cab48c9";
+
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+        .then(function(response) {
+            console.log(response)
+
+            console.log(response.name)
+            console.log(response.weather[0].icon)
+        })
+    }
 
 // Want to create a weather dashboard.
 // Left column is used to search for the weather in a specific city.
